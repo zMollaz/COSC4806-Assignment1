3753c65f6e34af5f51f98262bb9a9a8af5fae6c1
Ahmed Elmolla <87554450+zMollaz@users.noreply.github.com>
2024-05-14 14:52:49 +0000

    Reformatted login page layout and added proper html

--- login.php
+++ login.php
@@ -1,7 +1,23 @@
-<form action="/login.php" method="post">
-  <label for="username">Username:</label><br>
-  <input type="text" id="username" name="username" placeholder="Enter your username"><br>
-  <label for="password">Password:</label><br>
-  <input type="password" id="password" name="password" placeholder="Enter your password"><br><br>
-  <input type="submit" value="Submit">
-</form>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login</title>
+  </head>
+
+  <body>
+    <h1>Login</h1>
+    <form action="/login.php" method="post">
+      <label for="username">Username:</label>
+      <br>
+      <input type="text" id="username" name="username"                           placeholder="Enter your username">
+      <br>
+      <br>
+      <label for="password">Password:</label>
+      <br>
+      <input type="password" id="password"   name="password"                     placeholder="Enter your password">
+      <br>
+      <br>
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
